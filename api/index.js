4667189683ce2b8e618a5fec70b8fdbaf3430d96
zarulrohmadhan <85329803+zarulrohmadhan@users.noreply.github.com>
2021-07-23 21:30:05 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -25,7 +25,7 @@ state = 0;
 bot.onText(/\/predict/, (msg) => { 
     bot.sendMessage(
         msg.chat.id,
-        `Masukan nilai X1|X2|X3 contoh 9|9|9`
+        `Masukan nilai X1|X2|X3|X4 contoh 9|9|9|9`
     );   
     state = 1;
 });
@@ -36,11 +36,13 @@ bot.on('message', (msg) => {
         x1 = s[0]
         x2 = s[1]
         x3 = s[2]
+        x4 = s[3]
         model.predict(
             [
                 parseFloat(s[0]),
                 parseFloat(s[1]),
                 parseFloat(s[2])
+                parseFloat(s[3])
             ]
          ).then((jres)=>{
             bot.sendMessage(
@@ -55,6 +57,18 @@ bot.on('message', (msg) => {
                 msg.chat.id,
                 `Y3 yang diprediksi yaitu : ${jres[2]}`
             );
+              bot.sendMessage(
+                msg.chat.id,
+                `Y4 yang diprediksi yaitu : ${jres[3]}`
+            );
+              bot.sendMessage(
+                msg.chat.id,
+                `Y5 yang diprediksi yaitu : ${jres[4]}`
+            );
+              bot.sendMessage(
+                msg.chat.id,
+                `Y6 yang diprediksi yaitu : ${jres[5]}`
+            );
         })
      }else{
          state = 0
@@ -62,12 +76,13 @@ bot.on('message', (msg) => {
 })
     
 // routers
-r.get('/prediction/:x1/:x2/:x3', function(req, res, next) {    
+r.get('/prediction/:x1/:x2/:x3/:4', function(req, res, next) {    
     model.predict(
         [
             parseFloat(req.params.x1), // string to float
             parseFloat(req.params.x2),
             parseFloat(req.params.x3),
+            parseFloat(req.params.x4),
         ]
     ).then((jres)=>{
         res.json(jres);
